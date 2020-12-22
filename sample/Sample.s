b572b13cf4e5bec3917f0a9c18910c0c6fb615e5
Dmytro <dm.vict.kr@gmail.com>
2020-12-22 07:53:00 +0200

    Improve sample `Sample.s`

--- sample/Sample.s
+++ sample/Sample.s
@@ -1,5 +1,9 @@
-let _ = require( '../' )
+if( typeof module !== 'undefined' )
+require( '../' );
+
+let _ = _global_._test_;
 
 var result = _.squareRootOfSum( 14, 2 );
 
-console.log( result );
+console.log( `The square root of sum of 14 and 2 is : ${ result }` );
+/* log : The square root of sum of 14 and 2 is : 4 */
